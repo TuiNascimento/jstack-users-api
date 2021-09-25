1710cbc27b6701a8c1f8207cc154ddace73f70b9
Arthur Nascimento <arthur.nascimento@neomind.com.br>
2021-09-25 15:40:33 -0300

    first commit

---
+++ helpers/bodyParser.js
@@ -0,0 +1,16 @@
+const { request } = require("http");
+
+function bodyParser(request, callback){
+    let body = '';
+
+    request.on('data', (chunk) => {
+        body += chunk
+    });
+    request.on('end', () => {
+        request.body = JSON.parse(body);
+        callback();
+    });
+
+}
+
+module.exports = bodyParser
---
+++ mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Arthur'
+    },
+    {
+        id: 2,
+        name: 'Matheus'
+    },
+    {
+        id: 3,
+        name: 'João'
+    }
+]
